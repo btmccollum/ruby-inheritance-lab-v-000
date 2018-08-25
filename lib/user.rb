790609a7e7afd9fac757bb3c665a9e0b45da3a13
Brad McCollum <btmcco8@gmail.com>
2018-08-25 21:30:59 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,6 @@
 class User
-
   attr_accessor :first_name, :lastname
 
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
-  # end
   def initialize
     @knowledge = []
   end
