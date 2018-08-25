87a7c816d472f199a4d829146ada89a276e9ad52
Brad McCollum <btmcco8@gmail.com>
2018-08-25 21:29:05 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -6,6 +6,11 @@ class User
   #   @first_name = first_name
   #   @last_name = last_name
   # end
+  def initialize
+    @knowledge = []
+  end
+
+
   def first_name(first_name)
     @first_name = first_name
   end
